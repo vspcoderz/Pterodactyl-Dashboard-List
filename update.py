8cb4f558cb9309eff52d201137b9829d82961bd5
Vspcoder <165260098+vspcoderz@users.noreply.github.com>
2025-03-29 10:26:03 +0530

    Update update.py

--- update.py
+++ update.py
@@ -31,8 +31,7 @@ if __name__ == "__main__":
     table = generate_dashboard_table(dash_folder)
     
     # Prepare the full README content
-    readme_content = f"""
-# Pterodactyl-Dashboard-List
+    readme_content = f"""# Pterodactyl-Dashboard-List
 Want To Get Best Dashboard But Don't Know Where Are They? Here Is a List
 
 {table}
