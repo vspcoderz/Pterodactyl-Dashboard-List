2d1bfe79ed2973593df1a79d638f6c48cbeb8b16
Vspcoder <165260098+vspcoderz@users.noreply.github.com>
2025-04-03 18:38:43 +0530

    Update update.py

--- update.py
+++ update.py
@@ -31,73 +31,12 @@ if __name__ == "__main__":
     table = generate_dashboard_table(dash_folder)
     
     # Prepare the full README content
-    readme_content = f""" Hi There!
-# Pterodactyl-Dashboard-List
-Want To Get Best Dashboard But Don't Know Where Are They? Here Is a List
-
-{table}
-
-# How it works
-
-In Folder Dash Make a New Folder Named Your Dash In There Make a 2 Things `detail.json` And `README.md`  In README.md Put Details Of Your Dash And In detail.json Do like Following
-
-```json
-{
-  # Name Will Be Shown Of Folder Name
-  "owner": "Your Name",
-  "link": "https://your-dashboard-link.com", # If Not Have Then Put Your Discord Server Link
-  "github": "https://github.com/yourusername/repo",
-  "pricing": "Free",
-  "docs": "dash/my-awesome-dash/README.md" # Also Fill This Properly
-}
-
-```
-<pre>
-
-
-  
-</pre>
-
-
-
-# Star This Project!
-Star This Project And Share This To Any Hosting Startup How need help About It 
-
-# Send Pull Request to Add Yours
-
-## Upcoming ..
-I'll Make a Website With Docs To Install All Dash Easily
-
-- [ ] Proper List In Website
-- [ ] Docs
-
-## Milestone 🙌
-
-### Free Dashboard
-- [ ] 10 Dashboard Listed
-- [ ] 20 Dashboard Listed
-- [ ] 30 Dashboard Listed
-
-### Paid Dashboard
-- [ ] 1 Paid Dashboard Listed
-- [ ] 5 Paid Dashboard Listed
-- [ ] 10 Paid Dashboard Listed
-
-### Custom Dashboard (Which Is Made For Customer And It Will Show It)
-- [ ] 5 Custom Dashboard Listed
-
-### Stars
-- [ ] 10 Stars
-- [ ] 20 Stars
-- [ ] 50 Stars
-- [ ] 100 Stars
-- [ ] 200 Stars
-"""
+    readme_content = f""" {table} """
 
     # Save the complete README content to README.md
-    with open('README.md', 'w') as f:
+    with open('list.md', 'w') as f:
         f.write(readme_content)
 
-    print("README.md has been updated successfully.")
+    print("list.md has been updated successfully.")
 
 
